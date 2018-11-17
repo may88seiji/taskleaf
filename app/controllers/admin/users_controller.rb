7e39b198eab91cc33f3d544d768f174388069ff3
may88seiji <may88seiji@gmail.com>
2018-11-17 13:04:27 +0900

    add some files about admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,20 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(usr_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}を登録しました。」"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password, :passworkd_confirmation)
+  end
+end
