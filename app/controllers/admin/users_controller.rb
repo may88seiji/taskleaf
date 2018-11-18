82842cdb3d213f899ade3dbb6dd741c22bcabb8a
may88seiji <may88seiji@gmail.com>
2018-11-18 23:56:30 +0900

    finish chapter4

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,49 +1,55 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
-    @users = User.new
-  end
-
-  def edit
     @user = User.new
   end
 
+  def edit
+  end
 
   def create
-    @user = User.new(usr_params)
+    @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}を登録しました。」"
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
-      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
     else
       render :new
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}を削除しました。」"
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
   end
+
   private
+
   def user_params
-    params.require(:user).permit(:name, :email, :admin, :password, :password, :passworkd_confirmation)
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 end
